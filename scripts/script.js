b4b792f783233f21d1151f4572fa8262803b5690
Nahid Hossain <oikantik@gmail.com>
2020-11-11 20:26:06 -0500

    javascript part is done, will refactor later

--- scripts/script.js
+++ scripts/script.js
@@ -13,30 +13,37 @@ const weekDays = [
   {
     id: 0,
     name: "Sun",
+    fullName: "sunday",
   },
   {
     id: 1,
     name: "Mon",
+    fullName: "monday",
   },
   {
     id: 2,
     name: "Tue",
+    fullName: "tuesday",
   },
   {
     id: 3,
     name: "Wed",
+    fullName: "wednesday",
   },
   {
     id: 4,
     name: "Thu",
+    fullName: "thursday",
   },
   {
     id: 5,
     name: "Fri",
+    fullName: "friday",
   },
   {
     id: 6,
     name: "Sat",
+    fullName: "saturday",
   },
 ];
 
@@ -44,171 +51,121 @@ const months = [
   {
     id: 0,
     name: "January",
-    days: 31
+    days: 31,
   },
   {
     id: 1,
     name: "February",
-    days: new Date(getCurrentYear, 1, 29).getDate() === 29 ? 29 : 28
+    days: new Date(getCurrentYear, 1, 29).getDate() === 29 ? 29 : 28,
   },
   {
     id: 2,
     name: "March",
-    days: 31
+    days: 31,
   },
   {
     id: 3,
     name: "April",
-    days: 30
+    days: 30,
   },
   {
     id: 4,
     name: "May",
-    days: 31
+    days: 31,
   },
   {
     id: 5,
     name: "June",
-    days: 30
+    days: 30,
   },
   {
     id: 6,
     name: "July",
-    days: 31
+    days: 31,
   },
   {
     id: 7,
     name: "August",
-    days: 31
+    days: 31,
   },
   {
     id: 8,
     name: "September",
-    days: 30
+    days: 30,
   },
   {
     id: 9,
     name: "October",
-    days: 31
+    days: 31,
   },
   {
     id: 10,
     name: "November",
-    days: 30
+    days: 30,
   },
   {
     id: 11,
     name: "December",
-    days: 31
+    days: 31,
   },
 ];
 
-const currentMonth = date.getMonth(); // to get current month as current month starts from 0;
-const currentYear = date.getFullYear();
-const firstDayOfCurrentMonth = new Date(
-  `${currentMonth + 1}/1/${currentYear}`
-).getDay();
-const currentDay = weekDays.find(
-  (weekDay) => weekDay.id === firstDayOfCurrentMonth
-).name; // gives me the current day but what do I do with it?
 
-// current month's maximum days
 
-const maxDays = months.find(month => month.id === currentMonth);
-let countWednesday = 1;
+const addMonthsToDom = (currValue, maxNum, currDay) => {
+  let staticCurrValue = currValue;
+  console.log('curday >>', currDay)
+  while (staticCurrValue <= maxNum) {
+    document.querySelector(`#${currDay}`).innerHTML += `
+  <li class="days__day">${staticCurrValue}</li>
+  `;
+    staticCurrValue = staticCurrValue + 7;
+  }
 
-while(countWednesday <= maxDays.days) {
-  document.querySelector(`#wednesday`).innerHTML += `
-  <li class="days__day">${countWednesday}</li>
-  `
-  countWednesday = countWednesday + 7;
-}
+  // if this is the first month date, then get the weekdays from before, if any, put blank spaces for those
+  const indexOfCurrDay = weekDays.find((day) => day.fullName === currDay);
+  const arrayOfPreviousDays = weekDays.filter(
+    (day) => day.id < indexOfCurrDay.id
+  );
+  if (currValue === 1) {
+    arrayOfPreviousDays.forEach((day) => {
+      document.querySelector(`#${day.fullName}`).innerHTML += `
+      <li class="days__day"></li>
+      `;
+    });
+  }
 
-document.querySelector(`#tuesday`).innerHTML += `
-<li class="days__day"></li>
-`
-document.querySelector(`#monday`).innerHTML += `
-<li class="days__day"></li>
-`
-document.querySelector(`#sunday`).innerHTML += `
-<li class="days__day"></li>
-`
-
-document.querySelectorAll('#wednesday > li').length < 6 && (document.querySelector(`#wednesday`).innerHTML += `
+  document.querySelectorAll(`#${currDay} > li`).length < 6 &&
+    (document.querySelector(`#${currDay}`).innerHTML += `
 <li class="days__day"></li>
 `);
+};
 
-let countThursday = 2;
-while(countThursday <= maxDays.days) {
-  document.querySelector(`#thursday`).innerHTML += `
-  <li class="days__day">${countThursday}</li>
-  `
-  countThursday = countThursday + 7;
+
+const renderAllWeekdaysToDom = () => {
+  const currentMonth = date.getMonth(); // to get current month as current month starts from 0;
+  const currentYear = date.getFullYear();
+  const firstDateOfMonth = new Date(
+    `${currentMonth + 1}/1/${currentYear}`
+  ).getDay();
+  const firstDayOfCurrentMonth = weekDays.find(
+    (weekDay) => weekDay.id === firstDateOfMonth
+  ).fullName; // gives me the current day but what do I do with it?
+  console.log(firstDayOfCurrentMonth)
+  
+  // current month's maximum days
+  const maxDays = months.find((month) => month.id === currentMonth);
+  const copyOfWeekdays = [...new Set(weekDays)];
+  const spliced = copyOfWeekdays.splice(copyOfWeekdays.map(day => day.fullName).indexOf(firstDayOfCurrentMonth))
+  copyOfWeekdays.unshift(...spliced);
+  console.log("copyOfWeekdays>>", copyOfWeekdays);
+  copyOfWeekdays.forEach((day, index) => {
+    console.log(day)
+    addMonthsToDom(index + 1, maxDays.days, day.fullName)
+  })
 }
 
-document.querySelectorAll('#thursday > li').length < 6 && (document.querySelector(`#thursday`).innerHTML += `
-<li class="days__day"></li>
-`);
-
-let countFriday = 3;
-while(countFriday <= maxDays.days) {
-  document.querySelector(`#friday`).innerHTML += `
-  <li class="days__day">${countFriday}</li>
-  `
-  countFriday = countFriday + 7;
-}
-
-document.querySelectorAll('#friday > li').length < 6 && (document.querySelector(`#friday`).innerHTML += `
-<li class="days__day"></li>
-`);
-
-let countSaturday= 4;
-while(countSaturday <= maxDays.days) {
-  document.querySelector(`#saturday`).innerHTML += `
-  <li class="days__day">${countSaturday}</li>
-  `
-  countSaturday = countSaturday + 7;
-}
-
-document.querySelectorAll('#saturday > li').length < 6 && (document.querySelector(`#saturday`).innerHTML += `
-<li class="days__day"></li>
-`);
-
-let countSunday = 5;
-while(countSunday <= maxDays.days) {
-  document.querySelector(`#sunday`).innerHTML += `
-  <li class="days__day">${countSunday}</li>
-  `
-  countSunday = countSunday + 7;
-}
-
-document.querySelectorAll('#sunday > li').length < 6 && (document.querySelector(`#sunday`).innerHTML += `
-<li class="days__day"></li>
-`);
-
-let countMonday = 6;
-while(countMonday <= maxDays.days) {
-  document.querySelector(`#monday`).innerHTML += `
-  <li class="days__day">${countMonday}</li>
-  `
-  countMonday = countMonday + 7;
-}
-
-document.querySelectorAll('#monday > li').length < 6 && (document.querySelector(`#monday`).innerHTML += `
-<li class="days__day"></li>
-`);
-
-let countTuesday = 7;
-while(countTuesday <= maxDays.days) {
-  document.querySelector(`#tuesday`).innerHTML += `
-  <li class="days__day">${countTuesday}</li>
-  `
-  countTuesday = countTuesday + 7;
-}
-
-document.querySelectorAll('#tuesday > li').length < 6 && (document.querySelector(`#tuesday`).innerHTML += `
-<li class="days__day"></li>
-`);
+renderAllWeekdaysToDom();
 
 // what is the first day of any month? 1-1-2020
 // find the day for that and populate everything in that column right?
